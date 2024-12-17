ac132559cd10b7c4764439ca9a7d09a581b1c745
rohit <rohitsirsat855@gmail.com>
2024-12-17 22:42:41 +0530

    add all context

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -5,14 +5,19 @@ import App from "./App.jsx";
 import { ThemeProvider } from "./theme/themeContex";
 import { Toaster } from "./components/ui/toaster";
 import { BrowserRouter } from "react-router-dom";
-
+import { AuthProvider } from "./context/Auth/AuthContext";
+import { PostProvider } from "./context/Post/PostContext";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <App />
-        <Toaster />
-      </ThemeProvider>
+      <AuthProvider>
+        <PostProvider>
+          <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <App />
+            <Toaster />
+          </ThemeProvider>
+        </PostProvider>
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>
 );
